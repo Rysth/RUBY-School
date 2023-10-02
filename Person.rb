4f5c2965581dd86792c9da1e6b7231ef58429b18
Rysth <johnpalacios.t@gmail.com>
2023-10-02 11:44:44 -0500

    Finished Person class with public and private methods.

--- Person.rb
+++ Person.rb
@@ -1,27 +1,38 @@
 class Person
-  def initialize(id, name = 'Unknown', age, parent_permission = true)
+  def initialize(id, age, name = 'Unknown', parent_permission = true)
     @id = id
     @name = name
     @age = age
+    @parent_permission = parent_permission
   end
 
-  def id
+  def id?
     @id
   end
 
-  def name?(name)
+  def name!(name)
     @name = name
   end
 
-  def name
+  def name?
     @name
   end
 
-  def age?(age)
+  def age!(age)
     @age = age
   end
 
-  def age
+  def age?
     @age
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission 
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
   end
 end
