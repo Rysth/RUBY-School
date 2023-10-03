f705763dcb1b424ec18bf5c62a4c0c1fac771a02
Rysth <johnpalacios.t@gmail.com>
2023-10-03 11:12:14 -0500

    Fix Rubocop Linter errors.

--- decorators/trimmer_class.rb
+++ decorators/trimmer_class.rb
@@ -4,6 +4,7 @@ class TrimmerDecorator < Decorator
   # Implement and Override Decorator correct_name method
   def correct_name
     return @nameable.correct_name[0..9] if @nameable.correct_name.length > 10
+
     @nameable.correct_name
   end
 end
