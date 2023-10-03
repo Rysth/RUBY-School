711ba160c3bacde98f85bf1b3127c13efb1daa37
Rysth <johnpalacios.t@gmail.com>
2023-10-03 09:56:17 -0500

    Create Decorator class and inherits from Nameable with correct_name implemented.

---
+++ decorator_class.rb
@@ -0,0 +1,12 @@
+require_relative 'nameable_class'
+
+class Decorator < Nameable
+  def initialize
+    @nameable = Nameable.new
+  end
+
+  # Implement and Override Nameable correct_name method
+  def correct_name
+    @nameable.correct_name
+  end  
+end
