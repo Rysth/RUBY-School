3f299f35658df62098d9e426eb58a1f77fb979c8
Rysth <johnpalacios.t@gmail.com>
2023-10-03 11:11:36 -0500

    Update TrimmerDecorator to return the correct name.

--- decorators/trimmer_class.rb
+++ decorators/trimmer_class.rb
@@ -3,6 +3,7 @@ require_relative 'decorator_class'
 class TrimmerDecorator < Decorator
   # Implement and Override Decorator correct_name method
   def correct_name
-    @nameable.correct_name[0..9] if @nameable.correct_name.length > 10
+    return @nameable.correct_name[0..9] if @nameable.correct_name.length > 10
+    @nameable.correct_name
   end
 end
