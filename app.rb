015b1def2235f604469df2342b309d854d3e05f5
Rysth <johnpalacios.t@gmail.com>
2023-10-05 10:27:17 -0500

    Create method create_rental to match a book with a person.

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'teacher_class'
 require_relative 'associations/book_class'
 
 class App
-  LETTER_REGEX = /^[a-z]+$/i
+  LETTER_REGEX = /^[a-z ]+$/i
 
   def initialize
     @books = []
@@ -30,67 +30,65 @@ class App
   end
 
   def create_person
-    if @classrooms.empty?
-      person_type = age = 0
-      name = ''
-      until [1, 2].include?(person_type)
-        clear_screen
-        print "Do you want to create a STUDENT (1) or a TEACHER (2)? [Input the number]: "
-        person_type = gets.chomp.to_i
-        if person_type != 1 && person_type != 2
-          puts "Warning [INSERT 1 or 2]. [Press ENTER to continue]"
-          gets.chomp
-        end
-      end
-      until (1..119).cover?(age)
-        clear_screen
-        print "Age: "
-        age = gets.chomp.to_i
-        if age <= 0
-          puts "Insert a valid AGE. [Press ENTER to continue]"
-        end
-      end
-      until !name.empty? && name.match(LETTER_REGEX)
-        clear_screen
-        print "Name: "
-        name = gets.chomp.capitalize
-        if !name.match(LETTER_REGEX)
-          puts "Insert a valid NAME. [Press ENTER to continue]"
-          gets.chomp
-        end
-      end
-      case person_type
-        when 1
-          parent_acceptance = ''
-          until parent_acceptance == 'Y' || parent_acceptance == 'N'
-            clear_screen
-            print "Has parent permission? [Y/N]: "
-            parent_acceptance = gets.chomp.to_s.capitalize
-            if !parent_acceptance.match(LETTER_REGEX)
-              puts "Insert a valid ACCEPTANCE. [Press ENTER to continue]"
-              gets.chomp
-            end
-          end
-          permision = parent_acceptance == 'Y'
-          @people << Student.new(age, '', name, permision)
-        when 2
-          specialization = ''
-          until !specialization.empty? && specialization.match(LETTER_REGEX)
-            clear_screen
-            print "Specialization: "
-            specialization = gets.chomp.capitalize
-            if !specialization.match(LETTER_REGEX)
-              puts "Insert a valid SPECIALIZATION. [Press ENTER to continue]"
-              gets.chomp
-            end
-          end
-          @people << Teacher.new(age, specialization, name, true)
-      end
+    person_type = age = 0
+    name = ''
+    until [1, 2].include?(person_type)
       clear_screen
-      puts "Person created successfully!"
-    else
-      puts "You have to create at least 1 CLASSROOM before continue!"
+      puts "Welcome to the CREATE_PERSON method."
+      print "Do you want to create a STUDENT (1) or a TEACHER (2)? [Input the number]: "
+      person_type = gets.chomp.to_i
+      if person_type != 1 && person_type != 2
+        puts "Warning [INSERT 1 or 2]. [Press ENTER to continue]"
+        gets.chomp
+      end
     end
+    until (1..119).cover?(age)
+      clear_screen
+      print "Age: "
+      age = gets.chomp.to_i
+      if age <= 0
+        puts "Insert a valid AGE. [Press ENTER to continue]"
+      end
+    end
+    until !name.empty? && name.match(LETTER_REGEX)
+      clear_screen
+      print "Name: "
+      name = gets.chomp.capitalize.strip
+      if !name.match(LETTER_REGEX)
+        puts "Insert a valid NAME. [Press ENTER to continue]"
+        gets.chomp
+      end
+    end
+    case person_type
+      when 1
+        parent_acceptance = ''
+        until parent_acceptance == 'Y' || parent_acceptance == 'N'
+          clear_screen
+          print "Has parent permission? [Y/N]: "
+          parent_acceptance = gets.chomp.to_s.capitalize.strip
+          if !parent_acceptance.match(LETTER_REGEX)
+            puts "Insert a valid ACCEPTANCE. [Press ENTER to continue]"
+            gets.chomp
+          end
+        end
+        permision = parent_acceptance == 'Y'
+        @people << Student.new(age, '', name, permision)
+      when 2
+        specialization = ''
+        until !specialization.empty? && specialization.match(LETTER_REGEX)
+          clear_screen
+          print "Specialization: "
+          specialization = gets.chomp.capitalize.strip
+          if !specialization.match(LETTER_REGEX)
+            puts "Insert a valid SPECIALIZATION. [Press ENTER to continue]"
+            gets.chomp
+          end
+        end
+        @people << Teacher.new(age, specialization, name, true)
+    end
+    clear_screen
+    puts "Person created successfully! [Press ENTER to continue]"
+    gets.chomp
   end
 
   def clear_screen
@@ -101,9 +99,10 @@ class App
     title = author = ''
     until !title.empty? && title.match(LETTER_REGEX)
       clear_screen
+      puts "Welcome to the CREATE_BOOK method."
       print 'Title: '
-      title = gets.chomp.capitalize
-      if title.match(LETTER_REGEX)
+      title = gets.chomp.capitalize.strip
+      if !title.match(LETTER_REGEX)
         puts "Insert a valid TITLE. [Press ENTER to continue]"
         gets.chomp
       end
@@ -111,17 +110,46 @@ class App
     until !author.empty? && author.match(LETTER_REGEX)
       clear_screen
       print 'Author: '
-      author = gets.chomp.capitalize
-      if author.match(LETTER_REGEX)
+      author = gets.chomp.capitalize.strip
+      if !author.match(LETTER_REGEX)
         puts "Insert a valid AUTHOR. [Press ENTER to continue]"
         gets.chomp
       end
     end
     @books << Book.new(title, author)
-    puts "Book created successfully!"
+    puts "Book created successfully! [Press ENTER to continue]"
+    gets.chomp
+  end
+
+  def create_rental
+    if !@books.empty? && !@people.empty?
+      person_number = book_number = 0
+      until person_number != 0 && person_number > 0
+        clear_screen
+        puts "Welcome to the CREATE_RENTAL method."
+        list_people
+        print "Who are you?: [Write the NUMBER] "
+        person_number = gets.chomp.to_i
+      end
+      until book_number != 0 && book_number > 0
+        list_books
+        print "What book do you want?: [Write the NUMBER] "
+        book_number = gets.chomp.to_i
+      end
+      person_selected = @people[person_number-1]
+      book_selected = @books[book_number-1]
+      puts "Rental created successfully! [Press ENTER to continue]"
+      puts "----------------------------------"
+      puts "Person: #{person_selected.name} Book: #{book_selected.title}"
+      gets.chomp
+    else
+      puts "Please, add one BOOK and one PERSON. [Press ENTER to continue]"
+      gets.chomp
+    end
   end
 end
 
 app = App.new
 app.create_person
-app.list_people
+app.create_book
+app.create_rental
