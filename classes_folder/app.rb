8f9939c9deed73a06ca966d359e5dbffd271c7d2
Rysth <johnpalacios.t@gmail.com>
2023-10-10 11:47:59 -0500

    Create INPUT module to handle person and book (future)

--- classes_folder/app.rb
+++ classes_folder/app.rb
@@ -1,4 +1,5 @@
 require 'date'
+require_relative '../modules/input_module'
 require_relative '../modules/commands_module'
 require_relative '../modules/list_module'
 require_relative '../modules/menu_module'
@@ -57,75 +58,23 @@ class App
   end
 
   def create_person
-    person_type = choose_person_type
-    age = input_age
-    name = input_name
+    person_type = Input.choose_person_type
+    age = Input.input_age
+    name = Input.input_name
 
     case person_type
     when 1
-      parent_acceptance = input_parent_acceptance
+      parent_acceptance = Input.input_parent_acceptance
       permission = parent_acceptance == 'Y'
       @people << Student.new(age, '', name, permission)
     when 2
-      specialization = input_specialization
+      specialization = Input.input_specialization
       @people << Teacher.new(age, specialization, name, true)
     end
 
     Commands.clear_screen
     puts 'Person created successfully! [Press ENTER to continue]'
     gets.chomp
-  end
-
-  # Helper methods for input validation
-  def choose_person_type
-    person_type = 0
-    until [1, 2].include?(person_type)
-      Commands.clear_screen
-      puts 'Welcome to the CREATE_PERSON method.'
-      print 'Do you want to create a STUDENT (1) or a TEACHER (2)? [Input the number]: '
-      person_type = gets.chomp.to_i
-    end
-    person_type
-  end
-
-  def input_age
-    age = 0
-    until (1..120).cover?(age)
-      Commands.clear_screen
-      print "What's your age?: "
-      age = gets.chomp.to_i
-    end
-    age
-  end
-
-  def input_name
-    name = ''
-    until !name.empty? && name.match(LETTER_REGEX)
-      Commands.clear_screen
-      print "What's your name?: "
-      name = gets.chomp.capitalize.strip
-    end
-    name
-  end
-
-  def input_parent_acceptance
-    parent_acceptance = ''
-    until %w[Y N].include?(parent_acceptance)
-      Commands.clear_screen
-      print 'Has parent permission? [Y/N]: '
-      parent_acceptance = gets.chomp.to_s.capitalize.strip
-    end
-    parent_acceptance
-  end
-
-  def input_specialization
-    specialization = ''
-    until !specialization.empty? && specialization.match(LETTER_REGEX)
-      Commands.clear_screen
-      print "What's your specialization?: "
-      specialization = gets.chomp.capitalize.strip
-    end
-    specialization
   end
 
   def create_book
---
+++ modules/input_module.rb
@@ -0,0 +1,54 @@
+module Input
+  LETTER_REGEX = /^[a-z ]+$/i.freeze
+
+  def self.choose_person_type
+    person_type = 0
+    until [1, 2].include?(person_type)
+      Commands.clear_screen
+      puts 'Welcome to the CREATE_PERSON method.'
+      print 'Do you want to create a STUDENT (1) or a TEACHER (2)? [Input the number]: '
+      person_type = gets.chomp.to_i
+    end
+    person_type
+  end
+
+  def self.input_age
+    age = 0
+    until (1..120).cover?(age)
+      Commands.clear_screen
+      print "What's your age?: "
+      age = gets.chomp.to_i
+    end
+    age
+  end
+
+  def self.input_name
+    name = ''
+    until !name.empty? && name.match(LETTER_REGEX)
+      Commands.clear_screen
+      print "What's your name?: "
+      name = gets.chomp.capitalize.strip
+    end
+    name
+  end
+
+  def self.input_parent_acceptance
+    parent_acceptance = ''
+    until %w[Y N].include?(parent_acceptance)
+      Commands.clear_screen
+      print 'Has parent permission? [Y/N]: '
+      parent_acceptance = gets.chomp.to_s.capitalize.strip
+    end
+    parent_acceptance
+  end
+
+  def self.input_specialization
+    specialization = ''
+    until !specialization.empty? && specialization.match(LETTER_REGEX)
+      Commands.clear_screen
+      print "What's your specialization?: "
+      specialization = gets.chomp.capitalize.strip
+    end
+    specialization
+  end
+end
