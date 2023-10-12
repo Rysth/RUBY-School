d1961ddbfc93965a8a8e11963889501a13f83f47
Rysth <johnpalacios.t@gmail.com>
2023-10-12 13:01:18 -0500

    Add can_use_services? and add_rental method tests from Person class.

--- spec/person_class_spec.rb
+++ spec/person_class_spec.rb
@@ -1,4 +1,6 @@
 require_relative '../classes/person/person_class'
+require_relative '../associations/rental_class'
+require_relative '../classes/book/book_class'
 
 describe Person do
   context 'When valid input' do
@@ -11,10 +13,30 @@ describe Person do
         expect(person.parent_permission).to be true
       end
     end
+    describe 'using the can_use_services?() method' do
+      it 'create a new Person with age 16' do
+        person = Person.new(nil, 16, 'John', parent_permission: true)
+        expect(person.id).not_to be_nil
+        expect(person.name).to eql 'John'
+        expect(person.age).not_to be >= 18
+        expect(person.parent_permission).to be true
+        expect(person.can_use_services?).to be true
+      end
+    end
     describe 'using the correct_name() method' do
       it 'create a new Person with name John' do
         person = Person.new(nil, 18, 'John', parent_permission: false)
         expect(person.correct_name).to eql 'John'
+      end
+    end
+    describe 'using the add_rental() method' do
+      it 'creates a new Rental for a Person' do
+        date = Date.new(2023, 10, 12)
+        book = Book.new('B001', 'Test Book', 'John')
+        person = Person.new(nil, 20, 'John', parent_permission: true)
+        rental = Rental.new(date, book, person)
+        person.add_rental(rental)
+        expect(person.rentals).not_to be_nil
       end
     end
     describe 'using the to_json() method' do
@@ -35,10 +57,32 @@ describe Person do
         expect(person.parent_permission).to be true
       end
     end
+    describe 'using the can_use_services?() method' do
+      it 'create a new Person with age 16' do
+        person = Person.new(nil, 16, 'John', parent_permission: false)
+        expect(person.id).not_to be_nil
+        expect(person.name).to eql 'John'
+        expect(person.age).not_to be >= 18
+        expect(person.parent_permission).to be false
+        expect(person.can_use_services?).not_to be true
+      end
+    end
     describe 'using the correct_name() method' do
       it 'create a new Person with name John' do
         person = Person.new(nil, 18, nil, parent_permission: false)
         expect(person.correct_name).to be nil
+      end
+    end
+    describe 'using the add_rental() method' do
+      it 'creates a new Rental for a Person' do
+        date = Date.new(2023, 10, 12)
+        book = Book.new('B001', 'Test Book', 'John')
+        person = Person.new(nil, 50, 'John', parent_permission: true)
+        rental = Rental.new(date, book, person)
+        person.add_rental(rental)
+        book_two = Book.new('B002', 'Test Book', 'John')
+        rental_expect = Rental.new(date, book_two, person)
+        expect(person.rentals[0]).not_to eql(rental_expect)
       end
     end
     describe 'using the to_json() method' do
