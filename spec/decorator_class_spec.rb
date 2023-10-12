4370381ab8c2401894eb791a4f2526f6b5825c06
Rysth <johnpalacios.t@gmail.com>
2023-10-12 11:23:39 -0500

    Add Decorator class tests.

---
+++ spec/decorator_class_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../decorators/decorator_class'
+require_relative '../classes/nameable_class'
+
+describe Decorator do
+  # Create a dummy class for testing
+  class DummyNameable
+    attr_reader :name
+
+    def initialize(name)
+      @name = name
+    end
+
+    def correct_name
+      name
+    end
+  end
+
+  context 'when decorating an object' do
+    it 'delegates the correct_name method to the decorated object' do
+      dummy_nameable = DummyNameable.new('John Doe')
+      decorator = Decorator.new(dummy_nameable)
+      expect(decorator.correct_name).to eq('John Doe')
+    end
+
+    it 'delegates with a different input' do
+      dummy_nameable = DummyNameable.new('Jane Smith')
+      decorator = Decorator.new(dummy_nameable)
+      expect(decorator.correct_name).to eq('Jane Smith')
+    end
+  end
+end
