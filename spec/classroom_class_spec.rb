845b15ab7263649fa956379ff94c6ec4361cce5d
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-10-12 13:28:46 +0200

    Create tests for the Classroom class and its methods

--- spec/classroom_class_spec.rb
+++ spec/classroom_class_spec.rb
@@ -1,1 +1,22 @@
-require_relative '../classroom_class'
+require_relative '../associations/classroom_class'
+
+RSpec.describe Classroom do
+  let(:classroom) { Classroom.new('Green') }
+  let(:student) { instance_double('Student') }
+
+  describe '#initialize' do
+    it 'creates a new Classroom object with a label and no students' do
+      expect(classroom).to be_a(Classroom)
+      expect(classroom.label).to eq('Green')
+      expect(classroom.students).to be_empty
+    end
+  end
+
+  describe '#add_student' do
+    it 'adds a student to the classroom' do
+      expect(student).to receive(:add_classroom).with(classroom)
+      classroom.add_student(student)
+      expect(classroom.students).to include(student)
+    end
+  end
+end
